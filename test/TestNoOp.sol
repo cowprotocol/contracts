cc8061f38e15417afed1a5c37bba4107860da4fe
mfw78 <53399572+mfw78@users.noreply.github.com>
2024-06-04 10:23:18 +0000

    feat: foundry and devcontainer (#111)
    ## Description
    
    This PR:
    
    1. Adds [foundry](https://getfoundry.sh/) configuration for the
    contracts.
    2. Configures the solidity compiler for foundry to replicate that
    currently used within the contracts.
    3. Adds a devcontainer containing foundry and node (for still running
    hardhat during the migration phase).
    4. Configures a VS Code environment for solidity development.
    
    ## Test Plan
    
    1. Observe CI/CD continues to pass.
    
    ## Related Issues
    
    Closes #99
    Closes #98
    
    ---------
    
    Co-authored-by: Federico Giacon <58218759+fedgiac@users.noreply.github.com>

---
+++ test/TestNoOp.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.7.6;
+
+/// @dev No-op contract for start of forgifying contracts. Delete this!
+contract TestNoOp {
+    function test_noOp() external pure {}
+}
