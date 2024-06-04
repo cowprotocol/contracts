b59788cbe331777f9d81f8145cffa5ad6389f4da
mfw78 <mfw78@protonmail.com>
2024-06-04 15:24:02 +0000

    fix: license

--- test/TestNoOp.sol
+++ test/TestNoOp.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity >=0.7.6 <0.9.0;
 
 /// @dev No-op contract for start of forgifying contracts. Delete this!
