a507ee1424310df366046b8b216916caf124aa05
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-06-06 16:26:13 +0000

    chore(test): migrate GPv2SafeERC20 tests

--- test/GPv2SafeERC20/GPv2SafeERC20TestInterface.sol
+++ test/GPv2SafeERC20/GPv2SafeERC20TestInterface.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-pragma solidity >=0.7.6 <0.9.0;
-pragma abicoder v2;
+pragma solidity ^0.8.0;
 
-import "../interfaces/IERC20.sol";
-import "../libraries/GPv2SafeERC20.sol";
+import {IERC20} from "src/contracts/interfaces/IERC20.sol";
+import {GPv2SafeERC20} from "src/contracts/libraries/GPv2SafeERC20.sol";
 
 contract GPv2SafeERC20TestInterface {
     using GPv2SafeERC20 for IERC20;
