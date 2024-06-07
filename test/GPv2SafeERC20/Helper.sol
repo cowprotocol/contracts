b52a81e6f7b4898543a563fa76e82f6dbf4e4a91
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-06-07 12:17:45 +0000

    Revised file split following internal discussion

--- test/GPv2SafeERC20/Helper.sol
+++ test/GPv2SafeERC20/Helper.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import {Test} from "forge-std/Test.sol";
+
 import {IERC20} from "src/contracts/interfaces/IERC20.sol";
 import {GPv2SafeERC20} from "src/contracts/libraries/GPv2SafeERC20.sol";
 
-contract GPv2SafeERC20TestInterface {
+contract Harness {
     using GPv2SafeERC20 for IERC20;
 
     function transfer(IERC20 token, address to, uint256 value) public {
@@ -15,3 +17,13 @@ contract GPv2SafeERC20TestInterface {
         token.safeTransferFrom(from, to, value);
     }
 }
+
+contract Helper is Test {
+    Harness executor;
+    address recipient = makeAddr("TestHelper: recipient");
+
+    function setUp() public {
+        executor = new Harness();
+    }
+}
+
