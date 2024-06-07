bb22f18e352022f52c3a6cd4214e1c39567c4e48
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-06-07 09:36:06 +0000

    Break formatting

--- test/TestNoOp.sol
+++ test/TestNoOp.sol
@@ -3,5 +3,7 @@ pragma solidity >=0.7.6 <0.9.0;
 
 /// @dev No-op contract for start of forgifying contracts. Delete this!
 contract TestNoOp {
-    function test_noOp() external pure {}
+    function test_noOp(
+        
+    ) external pure {}
 }
