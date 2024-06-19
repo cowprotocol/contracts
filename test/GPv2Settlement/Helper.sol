70d61f5ff82888c142b5204730c8959fdfec50b2
mfw78 <mfw78@protonmail.com>
2024-06-19 12:27:15 +0000

    chore: settlement settle

---
+++ test/GPv2Settlement/Helper.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+import {Test, stdJson, Vm} from "forge-std/Test.sol";
+
+import {
+    IVault,
+    GPv2Authentication,
+    GPv2Settlement,
+    GPv2Trade,
+    GPv2Interaction,
+    GPv2Transfer,
+    IERC20
+} from "src/contracts/GPv2Settlement.sol";
+
+import {IVault, GPv2Authentication} from "src/contracts/GPv2Settlement.sol";
+import {GPv2AllowListAuthentication} from "src/contracts/GPv2AllowListAuthentication.sol";
+
+import {SettlementEncoder} from "test/libraries/encoders/SettlementEncoder.sol";
+import {SwapEncoder} from "test/libraries/encoders/SwapEncoder.sol";
+
+// solhint-disable func-name-mixedcase
+abstract contract Helper is Test {
+    using stdJson for string;
+
+    address internal deployer;
+    address internal owner;
+    Harness internal settlement;
+    bytes32 internal domainSeparator;
+    GPv2Authentication internal authenticator;
+    IVault internal vault;
+    GPv2AllowListAuthentication internal allowList;
+
+    SettlementEncoder.State internal encoder;
+    SwapEncoder.State internal swapEncoder;
+
+    address internal solver;
+    Vm.Wallet internal trader;
+
+    function setUp() public virtual {
+        // Configure addresses
+        deployer = makeAddr("deployer");
+        owner = makeAddr("owner");
+        solver = makeAddr("solver");
+        vm.startPrank(deployer);
+
+        // Deploy the allowlist manager
+        allowList = new GPv2AllowListAuthentication();
+        allowList.initializeManager(owner);
+        authenticator = allowList;
+
+        // Deploy the vault contract
+        vault = deployBalancerVault();
+
+        // Deploy the settlement contract
+        settlement = new Harness(authenticator, vault);
+
+        // Reset the prank
+        vm.stopPrank();
+
+        // By default, allow `solver` to settle
+        vm.prank(owner);
+        allowList.addSolver(solver);
+
+        // Configure default encoders
+        encoder = SettlementEncoder.makeSettlementEncoder(makeAddr("encoder"));
+        swapEncoder = SwapEncoder.makeSwapEncoder(makeAddr("swapEncoder"));
+
+        // Set the domain separator
+        domainSeparator = settlement.domainSeparator();
+
+        // Create wallets
+        trader = vm.createWallet("trader");
+    }
+
+    function deployBalancerVault() private returns (IVault vault_) {
+        string memory path = string.concat(vm.projectRoot(), "/", "balancer/Vault.json");
+        string memory json = vm.readFile(path);
+        bytes memory bytecode = json.parseRaw(".bytecode");
+
+        // solhint-disable-next-line no-inline-assembly
+        assembly ("memory-safe") {
+            vault_ := create(0, add(bytecode, 0x20), mload(bytecode))
+        }
+    }
+}
+
+contract Harness is GPv2Settlement {
+    constructor(GPv2Authentication authenticator_, IVault vault) GPv2Settlement(authenticator_, vault) {}
+
+    function setFilledAmount(bytes calldata orderUid, uint256 amount) external {
+        filledAmount[orderUid] = amount;
+    }
+
+    function computeTradeExecutionsTest(
+        IERC20[] calldata tokens,
+        uint256[] calldata clearingPrices,
+        GPv2Trade.Data[] calldata trades
+    ) external returns (GPv2Transfer.Data[] memory inTransfers, GPv2Transfer.Data[] memory outTransfers) {
+        (inTransfers, outTransfers) = computeTradeExecutions(tokens, clearingPrices, trades);
+    }
+
+    function computeTradeExecutionMemoryTest() external returns (uint256 mem) {
+        RecoveredOrder memory recoveredOrder;
+        GPv2Transfer.Data memory inTransfer;
+        GPv2Transfer.Data memory outTransfer;
+
+        // NOTE: Solidity stores the free memory pointer at address 0x40. Read
+        // it before and after calling `processOrder` to ensure that there are
+        // no memory allocations.
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            mem := mload(0x40)
+        }
+
+        recoveredOrder.data.validTo = uint32(block.timestamp);
+        computeTradeExecution(recoveredOrder, 1, 1, 0, inTransfer, outTransfer);
+
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            mem := sub(mload(0x40), mem)
+        }
+    }
+
+    function executeInteractionsTest(GPv2Interaction.Data[] calldata interactions) external {
+        executeInteractions(interactions);
+    }
+
+    function freeFilledAmountStorageTest(bytes[] calldata orderUids) external {
+        this.freeFilledAmountStorage(orderUids);
+    }
+
+    function freePreSignatureStorageTest(bytes[] calldata orderUids) external {
+        this.freePreSignatureStorage(orderUids);
+    }
+}
---
+++ test/GPv2Settlement/Settle/Settle.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+import {Vm} from "forge-std/Test.sol";
+
+import {Harness, Helper} from "../Helper.sol";
+
+import {GPv2Settlement, GPv2Interaction} from "src/contracts/GPv2Settlement.sol";
+
+import {Settlement} from "test/libraries/Settlement.sol";
+import {SettlementEncoder} from "test/libraries/encoders/SettlementEncoder.sol";
+
+// solhint-disable func-name-mixedcase
+contract Settle is Helper {
+    using Settlement for Harness;
+    using SettlementEncoder for SettlementEncoder.State;
+
+    function setUp() public override {
+        super.setUp();
+    }
+
+    function test_allowlist_rejects_transactions_from_non_solvers() public {
+        vm.expectRevert("GPv2: not a solver");
+        settlement.settle(encoder.encode(settlement));
+    }
+
+    function test_allowlist_accepts_transactions_from_solvers() public {
+        vm.prank(solver);
+        settlement.settle(encoder.encode(settlement));
+    }
+
+    function test_emits_a_settlement_event() public {
+        vm.expectEmit(address(settlement));
+        emit GPv2Settlement.Settlement(solver);
+        vm.prank(solver);
+        settlement.settle(encoder.encode(settlement));
+    }
+
+    function test_executes_interactions_stages_in_the_correct_order() public {
+        // Set the expected event emitted
+        vm.expectEmit(address(settlement));
+        add_indexed_interaction_helper(encoder, SettlementEncoder.InteractionStage.PRE);
+        add_indexed_interaction_helper(encoder, SettlementEncoder.InteractionStage.INTRA);
+        add_indexed_interaction_helper(encoder, SettlementEncoder.InteractionStage.POST);
+        emit GPv2Settlement.Settlement(solver);
+
+        // Now, start recording logs to prevent recording extraneous logs
+        vm.recordLogs();
+        vm.prank(solver);
+        settlement.settle(encoder.encode(settlement));
+
+        // Assert that the correct number of logs were recorded
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(entries.length, 4);
+    }
+
+    function add_indexed_interaction_helper(
+        SettlementEncoder.State storage encoder,
+        SettlementEncoder.InteractionStage stage
+    ) internal {
+        encoder.addInteraction(
+            GPv2Interaction.Data({target: address(0), value: 0, callData: abi.encode(bytes4(uint32(stage)))}), stage
+        );
+        emit GPv2Settlement.Interaction(address(0), 0, bytes4(uint32(stage)));
+    }
+
+    function test_reverts_if_encoded_interactions_has_incorrect_number_of_stages() public {
+        for (uint256 i = 1; i < 3; i++) {
+            GPv2Interaction.Data[][] memory interactions = new GPv2Interaction.Data[][](i * 2);
+            assertTrue(interactions.length != 3, "incorrect interaction array length test setup");
+            vm.expectRevert();
+            (bool revertsAsExpected,) = address(settlement).call(
+                abi.encodeWithSelector(
+                    GPv2Settlement.settle.selector, new bytes32[](0), new uint256[](0), new bytes[](0), interactions
+                )
+            );
+            assertTrue(revertsAsExpected, "incorrect interaction array length did not revert");
+        }
+    }
+
+    function empty_interaction() internal pure returns (GPv2Interaction.Data memory) {
+        return GPv2Interaction.Data({target: address(0), value: 0, callData: ""});
+    }
+}
