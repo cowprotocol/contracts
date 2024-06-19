cab11cb2fb9cef1ab24110990e1c1bb7af0f3bb1
mfw78 <mfw78@protonmail.com>
2024-06-19 12:25:13 +0000

    chore: deployment variable and contract initialisation

---
+++ test/GPv2Settlement/DeploymentParams.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+import {GPv2VaultRelayer} from "src/contracts/GPv2VaultRelayer.sol";
+import {Bytecode} from "test/libraries/Bytecode.sol";
+
+import {Helper, Vm} from "./Helper.sol";
+
+// solhint-disable func-name-mixedcase
+contract DeploymentParams is Helper {
+    using Bytecode for Vm;
+    using Bytecode for bytes;
+
+    function test_settlement_sets_authenticator() public view {
+        assertEq(address(authenticator), address(settlement.authenticator()));
+    }
+
+    function test_settlement_sets_vault() public view {
+        assertEq(address(vault), address(settlement.vault()));
+    }
+
+    function test_settlement_deployment_deploys_a_vault() public view {
+        GPv2VaultRelayer relayer = GPv2VaultRelayer(settlement.vaultRelayer());
+        bytes memory deployedBytecode = address(relayer).code;
+        assertEq(deployedBytecode.toMetadata(), type(GPv2VaultRelayer).creationCode.toMetadata());
+    }
+
+    function test_settlement_deployment_sets_vault_relayer_immutables() public view {
+        bytes[] memory rawImmutables = vm.deployedImmutables("GPv2VaultRelayer", address(settlement.vaultRelayer()));
+        assertEq(rawImmutables.length, 2, "Invalid number of immutables");
+        assertEq(rawImmutables[0], abi.encode(address(settlement)), "invalid creator address");
+        assertEq(rawImmutables[1], abi.encode(address(vault)), "invalid vault address");
+    }
+}
