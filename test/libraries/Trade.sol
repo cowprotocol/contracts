7fb81d0887297d5a5fb691a961b5699006394cd3
mfw78 <53399572+mfw78@users.noreply.github.com>
2024-07-11 11:57:53 +0000

    chore: swap encoder and settlement library (#167)
    ## Description
    
    This PR follows on from #163, including a `SwapEncoder`, and
    `Settlement` helper library. In completing the encoders, the main
    encoding functions have converged on `encode` as a generic term
    (inheriting context from the caller).
    
    One issue that came about during the development is in order for foundry
    test suites to work expectedly with `vm.expectRevert` etc, the very next
    _non `vm`_ call must be to the contract expected to revert. This did not
    allow for calling encoding functions within the settle function, and
    would result in additional complexity with the test suite.
    
    To combat this, the encoders were refactored to libraries, making use of
    `SLOAD`, and therefore storing their respective encoder data within the
    test contract's state - now ensuring a consistent `vm.expectRevert`
    experience and cleaner test suites.
    
    Additionally, imports across the libraries are cleaned.
    
    ## Test Plan
    
    1. Review code and contrast with `ts` libraries.
    
    ## Related Issues
    
    Closes #113 
    Supersedes #164
    
    ---------
    
    Co-authored-by: Federico Giacon <58218759+fedgiac@users.noreply.github.com>

--- test/libraries/Trade.sol
+++ test/libraries/Trade.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.8.26;
 
-import {IERC20} from "src/contracts/interfaces/IERC20.sol";
-import {GPv2Signing} from "src/contracts/mixins/GPv2Signing.sol";
-import {GPv2Order} from "src/contracts/libraries/GPv2Order.sol";
-import {GPv2Trade} from "src/contracts/libraries/GPv2Trade.sol";
+import {IERC20, GPv2Order, GPv2Trade, GPv2Signing} from "src/contracts/mixins/GPv2Signing.sol";
 import {Sign} from "./Sign.sol";
 import {Order} from "./Order.sol";
 
@@ -16,7 +13,6 @@ library Trade {
     using Sign for uint256;
 
     error TokenIndexOutOfBounds();
-    error TokenNotFound();
 
     /// Trade flags
     struct Flags {
@@ -24,25 +20,29 @@ library Trade {
         GPv2Signing.Scheme signingScheme;
     }
 
+    /// @dev Given a `flags` struct, encode it into a uint256 for a GPv2Trade
     function toUint256(Flags memory flags) internal pure returns (uint256 encodedFlags) {
         encodedFlags |= flags.flags.toUint256();
         encodedFlags |= flags.signingScheme.toUint256();
     }
 
+    /// @dev Given a GPv2Trade encoded flags, decode them into a `Flags` struct
     function toFlags(uint256 encodedFlags) internal pure returns (Flags memory flags) {
         flags.flags = encodedFlags.toFlags();
         flags.signingScheme = encodedFlags.toSigningScheme();
     }
 
+    /// @dev Given a signature, executed amount and tokens, encode them into a GPv2Trade
     function toTrade(
         GPv2Order.Data memory order,
-        IERC20[] memory tokens,
+        uint256 sellTokenIndex,
+        uint256 buyTokenIndex,
         Sign.Signature memory signature,
         uint256 executedAmount
     ) internal pure returns (GPv2Trade.Data memory trade) {
         trade = GPv2Trade.Data({
-            sellTokenIndex: findTokenIndex(order.sellToken, tokens),
-            buyTokenIndex: findTokenIndex(order.buyToken, tokens),
+            sellTokenIndex: sellTokenIndex,
+            buyTokenIndex: buyTokenIndex,
             receiver: order.receiver,
             sellAmount: order.sellAmount,
             buyAmount: order.buyAmount,
@@ -65,6 +65,7 @@ library Trade {
         });
     }
 
+    /// @dev Given a trade and tokens, encode them into a GPv2Order
     function toOrder(GPv2Trade.Data memory trade, IERC20[] memory tokens)
         internal
         pure
@@ -90,14 +91,5 @@ library Trade {
             buyTokenBalance: flags.buyTokenBalance,
             partiallyFillable: flags.partiallyFillable
         });
-    }
-
-    function findTokenIndex(IERC20 token, IERC20[] memory tokens) private pure returns (uint256) {
-        for (uint256 i = 0; i < tokens.length; i++) {
-            if (tokens[i] == token) {
-                return i;
-            }
-        }
-        revert TokenNotFound();
     }
 }
---
+++ test/libraries/encoders/SettlementEncoder.sol
@@ -0,0 +1,235 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity ^0.8.26;
+
+import {Vm} from "forge-std/Test.sol";
+
+import {
+    IERC20,
+    GPv2Order,
+    GPv2Trade,
+    GPv2Signing,
+    GPv2Interaction,
+    GPv2Settlement
+} from "src/contracts/GPv2Settlement.sol";
+
+import {Sign} from "../Sign.sol";
+import {Trade} from "../Trade.sol";
+
+import {Registry, TokenRegistry} from "./TokenRegistry.sol";
+
+library SettlementEncoder {
+    using GPv2Order for GPv2Order.Data;
+    using Trade for GPv2Order.Data;
+    using Sign for Vm;
+    using TokenRegistry for TokenRegistry.State;
+    using TokenRegistry for Registry;
+
+    /// The stage an interaction should be executed in
+    enum InteractionStage {
+        PRE,
+        INTRA,
+        POST
+    }
+
+    /**
+     * Order refund data.
+     *
+     * @dev after the London hardfork (specifically the introduction of EIP-3529)
+     * order refunds have become meaningless as the refunded amount is less than the
+     * gas cost of triggering the refund. The logic surrounding this feature is kept
+     * in order to keep full test coverage and in case the value of a refund will be
+     * increased again in the future. However, order refunds should not be used in
+     * an actual settlement.
+     */
+    struct OrderRefunds {
+        bytes[] filledAmounts;
+        bytes[] preSignatures;
+    }
+
+    /// Encoded settlement parameters
+    struct EncodedSettlement {
+        IERC20[] tokens;
+        uint256[] clearingPrices;
+        GPv2Trade.Data[] trades;
+        GPv2Interaction.Data[][3] interactions;
+    }
+
+    /// State for the settlement encoder (a stateful library)
+    struct State {
+        Registry tokenRegistry;
+        GPv2Trade.Data[] trades;
+        GPv2Interaction.Data[][3] interactions;
+        OrderRefunds refunds;
+    }
+
+    bytes32 internal constant STATE_STORAGE_SLOT = keccak256("SettlementEncoder.storage");
+
+    /// @dev Make a new settlement encoder, bumping the nonce
+    function makeSettlementEncoder() internal returns (State storage state) {
+        uint256 nonce;
+        bytes32 nonceSlot = STATE_STORAGE_SLOT;
+        assembly {
+            nonce := sload(nonceSlot)
+            sstore(nonceSlot, add(nonce, 1))
+        }
+        bytes32 slot = keccak256(abi.encodePacked(STATE_STORAGE_SLOT, nonce));
+        assembly {
+            state.slot := slot
+        }
+
+        setTokenRegistry(state, Registry.wrap(keccak256(abi.encode(slot))));
+    }
+
+    /// @dev Allow an arbitrary token registry to be set
+    function setTokenRegistry(State storage state, Registry registry) internal {
+        state.tokenRegistry = registry;
+    }
+
+    /// @dev Add a token to the token registry
+    function addToken(State storage state, IERC20 token) internal {
+        state.tokenRegistry.tokenRegistry().pushIfNotPresentIndexOf(token);
+    }
+
+    /// @dev Retrieve all the tokens in the token registry
+    function tokens(State storage state) internal returns (IERC20[] memory) {
+        TokenRegistry.State storage tokenRegistry = state.tokenRegistry.tokenRegistry();
+        return tokenRegistry.getTokens();
+    }
+
+    /// @dev Automaticatlly append all the order refunds to the POST interactions and return all the interactions
+    function interactions(State storage state, address settlement)
+        internal
+        view
+        returns (GPv2Interaction.Data[][3] memory)
+    {
+        GPv2Interaction.Data[] memory r = encodeOrderRefunds(state, settlement);
+
+        // All the order refunds are encoded in the POST interactions so we take some liberty and
+        // use a short variable to represent the POST stage.
+        uint256 POST = uint256(InteractionStage.POST);
+        GPv2Interaction.Data[] memory postInteractions =
+            new GPv2Interaction.Data[](state.interactions[POST].length + r.length);
+
+        for (uint256 i = 0; i < state.interactions[POST].length; i++) {
+            postInteractions[i] = state.interactions[POST][i];
+        }
+
+        for (uint256 i = 0; i < r.length; i++) {
+            postInteractions[state.interactions[POST].length + i] = r[i];
+        }
+
+        return [
+            state.interactions[uint256(InteractionStage.PRE)],
+            state.interactions[uint256(InteractionStage.INTRA)],
+            postInteractions
+        ];
+    }
+
+    /// @dev Add a trade to the state with the given order, signature, and executed amount
+    function encodeTrade(
+        State storage state,
+        GPv2Order.Data memory order,
+        Sign.Signature memory signature,
+        uint256 executedAmount
+    ) internal {
+        (uint256 sellTokenIndex, uint256 buyTokenIndex) =
+            TokenRegistry.tokenIndices(state.tokenRegistry.tokenRegistry(), order);
+        state.trades.push(order.toTrade(sellTokenIndex, buyTokenIndex, signature, executedAmount));
+    }
+
+    /// @dev Sign and encode a trade
+    function signEncodeTrade(
+        State storage state,
+        Vm vm,
+        Vm.Wallet memory owner,
+        GPv2Order.Data memory order,
+        bytes32 domainSeparator,
+        GPv2Signing.Scheme signingScheme,
+        uint256 executedAmount
+    ) internal {
+        Sign.Signature memory signature = vm.sign(owner, order, signingScheme, domainSeparator);
+        encodeTrade(state, order, signature, executedAmount);
+    }
+
+    /// @dev Append an interaction to the state at the specified stage
+    function addInteraction(State storage state, GPv2Interaction.Data memory interaction, InteractionStage stage)
+        internal
+    {
+        state.interactions[uint256(stage)].push(interaction);
+    }
+
+    /// @dev Append order funds to the state
+    function addOrderRefunds(State storage state, OrderRefunds memory orderRefunds) internal {
+        if (orderRefunds.filledAmounts.length > 0) {
+            for (uint256 i = 0; i < orderRefunds.filledAmounts.length; i++) {
+                bytes memory filledAmount = orderRefunds.filledAmounts[i];
+                if (filledAmount.length != GPv2Order.UID_LENGTH) {
+                    revert("Invalid order UID length");
+                }
+                state.refunds.filledAmounts.push(filledAmount);
+            }
+        }
+
+        if (orderRefunds.preSignatures.length > 0) {
+            for (uint256 i = 0; i < orderRefunds.preSignatures.length; i++) {
+                bytes memory preSignature = orderRefunds.preSignatures[i];
+                if (preSignature.length != GPv2Order.UID_LENGTH) {
+                    revert("Invalid order UID length");
+                }
+                state.refunds.preSignatures.push(preSignature);
+            }
+        }
+    }
+
+    /// @dev Encode the current state into a settlement
+    function encode(State storage state, GPv2Settlement settlement) internal returns (EncodedSettlement memory) {
+        return EncodedSettlement({
+            tokens: tokens(state),
+            clearingPrices: state.tokenRegistry.tokenRegistry().clearingPrices(),
+            trades: state.trades,
+            interactions: interactions(state, address(settlement))
+        });
+    }
+
+    /// @dev Encode just the setup interactions and use an otherwise empty encoded settlement
+    function encode(State storage, GPv2Interaction.Data[] memory setupInteractions)
+        internal
+        pure
+        returns (EncodedSettlement memory encodedSettlement)
+    {
+        encodedSettlement.interactions[uint256(InteractionStage.INTRA)] = setupInteractions;
+    }
+
+    /// @dev Encode the order refunds into interactions
+    function encodeOrderRefunds(State storage state, address settlement)
+        private
+        view
+        returns (GPv2Interaction.Data[] memory refunds_)
+    {
+        uint256 numInteractions =
+            (state.refunds.filledAmounts.length > 0 ? 1 : 0) + (state.refunds.preSignatures.length > 0 ? 1 : 0);
+        refunds_ = new GPv2Interaction.Data[](numInteractions);
+
+        uint256 i = 0;
+        if (state.refunds.filledAmounts.length > 0) {
+            refunds_[i++] = refundFnEncoder(
+                settlement, GPv2Settlement.freeFilledAmountStorage.selector, state.refunds.filledAmounts
+            );
+        }
+
+        if (state.refunds.preSignatures.length > 0) {
+            refunds_[i] = refundFnEncoder(
+                settlement, GPv2Settlement.freePreSignatureStorage.selector, state.refunds.preSignatures
+            );
+        }
+    }
+
+    /// @dev Encode a refund function call
+    function refundFnEncoder(address settlement, bytes4 fn, bytes[] memory orderUids)
+        private
+        pure
+        returns (GPv2Interaction.Data memory)
+    {
+        return GPv2Interaction.Data({target: settlement, value: 0, callData: abi.encodeWithSelector(fn, orderUids)});
+    }
+}
