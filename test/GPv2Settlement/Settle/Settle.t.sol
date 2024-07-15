dc75dd204734111b23d8663f4c5b3abdaa5b79f1
mfw78 <mfw78@protonmail.com>
2024-07-15 07:33:57 +0000

    chore: remove interaction ordering tests

--- test/GPv2Settlement/Settle/Settle.t.sol
+++ test/GPv2Settlement/Settle/Settle.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.8.26;
 
-import {Harness, Helper} from "../Helper.sol";
+import {Helper} from "../Helper.sol";
 
 import {GPv2Settlement, GPv2Interaction} from "src/contracts/GPv2Settlement.sol";
 
@@ -25,38 +25,7 @@ contract Settle is Helper {
         vm.expectEmit(address(settlement));
         emit GPv2Settlement.Settlement(solver);
         vm.prank(solver);
-        settlement.settle(encoder.encode(settlement));
-    }
-
-    function test_executes_interactions_stages_in_the_correct_order() public {
-        // Set the expected event emitted
-        vm.expectEmit(address(settlement));
-        add_indexed_interaction_helper(encoder, SettlementEncoder.InteractionStage.PRE);
-        add_indexed_interaction_helper(encoder, SettlementEncoder.InteractionStage.INTRA);
-        add_indexed_interaction_helper(encoder, SettlementEncoder.InteractionStage.POST);
-        emit GPv2Settlement.Settlement(solver);
-
-        // Now, start recording logs to prevent recording extraneous logs
-        vm.recordLogs();
-        vm.prank(solver);
-        settlement.settle(encoder.encode(settlement));
-
-        // Assert that the correct number of logs were recorded
-        Vm.Log[] memory entries = vm.getRecordedLogs();
-        assertEq(entries.length, 4);
-        assertEq(entries[0].data, abi.encodePacked(bytes32(bytes20(address(0))), bytes32(bytes8(uint8(SettlementEncoder.InteractionStage.PRE)))));
-        assertEq(entries[1].data, abi.encodePacked(bytes32(bytes20(address(0))), bytes32(bytes8(uint8(SettlementEncoder.InteractionStage.INTRA)))));
-        assertEq(entries[2].data, abi.encodePacked(bytes32(bytes20(address(0))), bytes32(bytes8(uint8(SettlementEncoder.InteractionStage.POST)))));
-    }
-
-    function add_indexed_interaction_helper(
-        SettlementEncoder.State storage encoder,
-        SettlementEncoder.InteractionStage stage
-    ) internal {
-        encoder.addInteraction(
-            GPv2Interaction.Data({target: address(0), value: 0, callData: abi.encode(bytes4(uint32(stage)))}), stage
-        );
-        emit GPv2Settlement.Interaction(address(0), 0, bytes4(uint32(stage)));
+        settle(encoder.encode(settlement));
     }
 
     function test_reverts_if_encoded_interactions_has_incorrect_number_of_stages() public {
