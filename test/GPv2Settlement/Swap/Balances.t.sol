a4e7bfbfedda8e3252a858ce13b7f38479ba0ec4
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-08-07 07:35:08 +0000

    Invert buy and sell balance in naming

--- test/GPv2Settlement/Swap/Balances.t.sol
+++ test/GPv2Settlement/Swap/Balances.t.sol
@@ -11,31 +11,31 @@ import {SwapEncoder} from "test/libraries/encoders/SwapEncoder.sol";
 contract Balances is Helper {
     using SwapEncoder for SwapEncoder.State;
 
-    function test_performs_a_swap_from_erc20_to_erc20_balance_when_specified() public {
+    function test_performs_a_swap_to_sell_erc20_from_buy_erc20_when_specified() public {
         performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_ERC20, GPv2Order.BALANCE_ERC20);
     }
 
-    function test_performs_a_swap_from_internal_to_erc20_balance_when_specified() public {
-        performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_INTERNAL, GPv2Order.BALANCE_ERC20);
-    }
-
-    function test_performs_a_swap_from_erc20_to_external_balance_when_specified() public {
-        performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_ERC20, GPv2Order.BALANCE_EXTERNAL);
-    }
-
-    function test_performs_a_swap_from_internal_to_external_balance_when_specified() public {
-        performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_INTERNAL, GPv2Order.BALANCE_EXTERNAL);
-    }
-
-    function test_performs_a_swap_from_erc20_to_internal_balance_when_specified() public {
+    function test_performs_a_swap_to_sell_erc20_from_buy_internal_when_specified() public {
         performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_ERC20, GPv2Order.BALANCE_INTERNAL);
     }
 
-    function test_performs_a_swap_from_internal_to_internal_balance_when_specified() public {
+    function test_performs_a_swap_to_sell_external_from_buy_erc20_when_specified() public {
+        performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_EXTERNAL, GPv2Order.BALANCE_ERC20);
+    }
+
+    function test_performs_a_swap_to_sell_external_from_buy_internal_when_specified() public {
+        performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_EXTERNAL, GPv2Order.BALANCE_INTERNAL);
+    }
+
+    function test_performs_a_swap_to_sell_internal_from_buy_erc20_when_specified() public {
+        performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_INTERNAL, GPv2Order.BALANCE_ERC20);
+    }
+
+    function test_performs_a_swap_to_sell_internal_from_buy_internal_when_specified() public {
         performs_a_swap_with_the_specified_balances(GPv2Order.BALANCE_INTERNAL, GPv2Order.BALANCE_INTERNAL);
     }
 
-    function performs_a_swap_with_the_specified_balances(bytes32 buyTokenBalance, bytes32 sellTokenBalance) private {
+    function performs_a_swap_with_the_specified_balances(bytes32 sellTokenBalance, bytes32 buyTokenBalance) private {
         Vm.Wallet memory trader = vm.createWallet("trader");
         address payable receiver = payable(makeAddr("receiver"));
         IERC20 sellToken = IERC20(makeAddr("sell token"));
