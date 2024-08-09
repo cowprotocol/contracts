d0173f0494ad84aaf71855f6a6c2f2fd79f284b5
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-08-09 07:20:43 +0000

    chore: migrate GPv2Signing domain separator tests to Foundry

---
+++ test/GPv2Signing/DomainSeparator.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+pragma solidity ^0.8;
+
+import {Helper} from "./Helper.sol";
+
+import {Harness, Helper} from "./Helper.sol";
+import {Eip712} from "test/libraries/Eip712.sol";
+import {Order as OrderLib} from "test/libraries/Order.sol";
+
+contract DomainSeparator is Helper {
+    function test_TYPE_HASH_matches_the_EIP_712_order_type_hash() public view {
+        bytes32 expectedDomainSeparator = Eip712.hashStruct(
+            Eip712.EIP712Domain({
+                name: "Gnosis Protocol",
+                version: "v2",
+                chainId: block.chainid,
+                verifyingContract: address(executor)
+            })
+        );
+        assertEq(executor.domainSeparator(), expectedDomainSeparator);
+    }
+
+    function test_should_have_a_different_replay_protection_for_each_deployment() public {
+        Harness signing = new Harness();
+        assertNotEq(executor.domainSeparator(), signing.domainSeparator());
+    }
+}
