bc7f235db4dc7d7014a485305c1e6fc3cad59479
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-08-12 06:58:16 +0000

    Remove duplicated import

--- test/GPv2Signing/DomainSeparator.t.sol
+++ test/GPv2Signing/DomainSeparator.t.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.8;
 
-import {Helper} from "./Helper.sol";
-
 import {Harness, Helper} from "./Helper.sol";
 import {Eip712} from "test/libraries/Eip712.sol";
 
