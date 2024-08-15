af90c59e4c9e1f6f8d71c83c6000c6666347ede0
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2024-08-15 15:42:14 +0000

    chore: migrate GPv2Signing recoverOrderSigner tests to Foundry (#207)
    ## Description
    
    See title.
    
    `test_reverts_for_invalid_signing_schemes` has not been migrated because
    the compiler itself enforces that the signing scheme is valid and
    there's no way that I know of to force the compiler to give the library
    a value that doesn't fit the expected input type.
    
    ## Test Plan
    
    CI.
    
    ## Related Issues
    
    #120
    
    ---------
    
    Co-authored-by: mfw78 <53399572+mfw78@users.noreply.github.com>

--- test/GPv2Signing/Helper.sol
+++ test/GPv2Signing/Helper.sol
@@ -3,12 +3,23 @@ pragma solidity ^0.8;
 
 import {Test} from "forge-std/Test.sol";
 
+import {GPv2Order} from "src/contracts/mixins/GPv2Signing.sol";
+
+import {Sign} from "test/libraries/Sign.sol";
 import {GPv2SigningTestInterface} from "test/src/GPv2SigningTestInterface.sol";
 
 // TODO: move the content of `GPv2SigningTestInterface` here once all tests have
 // been removed.
 // solhint-disable-next-line no-empty-blocks
-contract Harness is GPv2SigningTestInterface {}
+contract Harness is GPv2SigningTestInterface {
+    function recoverOrderSignerTest(GPv2Order.Data memory order, Sign.Signature calldata signature)
+        public
+        view
+        returns (address owner)
+    {
+        (, owner) = recoverOrderSigner(order, signature.scheme, signature.data);
+    }
+}
 
 contract Helper is Test {
     Harness internal executor;
