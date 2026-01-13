8836ac18b34fc0385f8073213816ba483c26f267
Kaze <kaze@cow.fi>
2026-01-13 21:07:10 +0900

    more unnecessary change undos and clarify vaultrelayer issue

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,13 +1,13 @@
 import { setupArtifactsTasks } from "./artifacts";
 import { setupDecodeTask } from "./decode";
 import { setupSolversTask } from "./solvers";
-//import { setupTenderlyTask } from "./tenderly";
+import { setupTenderlyTask } from "./tenderly";
 import { setupWithdrawTask } from "./withdraw";
 
 export function setupTasks(): void {
   setupArtifactsTasks();
   setupDecodeTask();
   setupSolversTask();
-  //setupTenderlyTask();
+  setupTenderlyTask();
   setupWithdrawTask();
 }
